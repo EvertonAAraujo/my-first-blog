7f6c99012a39bae961b0fd9a4963c490cfd1bb11
EvertonAAraujo <everton.aaraujo@outlook.com>
2017-11-21 16:59:14 -0200

    Adicionado campos

--- loja/polls/admin.py
+++ loja/polls/admin.py
@@ -1,21 +1,31 @@
 from django.contrib import admin
+from django.contrib.admin.templatetags.admin_list import date_hierarchy
+
 from .models import Question, Choice, Cliente, Produto, Venda
 
 #admin.site.register(Question)
 #admin.site.register(Choice)
 
 class VendaAdmin(admin.ModelAdmin):
-    raw_id_fields = ['cliente', 'produto']
-    list_display = ['cliente', 'produto', 'quantidade', 'valor', 'data', 'atualizacao']
+    #raw_id_fields = ['cliente', 'produto']#mostra por id os campos
+    list_display = ['cliente', 'produto', 'quantidade', 'valor', 'data', 'atualizacao']#campos exibidos na pagina de venda
+    list_display_links = ['produto', 'cliente']#campos que são links para editar a venda
+    list_editable = ['quantidade']#edita o campo direto na tela. O Campo não pode ser link
     readonly_fields = ['valor']
     readonly_fields = ['atualizacao']
-
+    list_filter = ['cliente', 'produto']
+    list_per_page = 3 #lista a quantidade de resultados por pagina
+    ordering = readonly_fields
+    preserve_filters = False
+    #radio_fields = {"grupo": admin.VERTICAL}
 
 class ProdutoAdmin(admin.ModelAdmin):
     search_fields = ['produto']
 
 class ClienteAdmin(admin.ModelAdmin):
     search_fields = ['nome']
+    save_as = True#Se true salva como novo em vez de editar
+    save_on_top = True
 
 
 admin.site.register(Venda, VendaAdmin)
